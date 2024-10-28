c837efac5c7f184089375fa622e430ca5084fa19
namdahyeon <61017675+namdahyeon@users.noreply.github.com>
2024-10-28 18:27:43 +0900

    리액트앱 업데이트

---
+++ src/App-css.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import Form from "./components/Form";
+
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      conpleted: false,
+    };
+
+    //원래 있던 할 일에 새로운 할 일을 더해주기
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue(""); //입력란 텍스트 삭제
+  };
+
+  return (
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
+        </div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <List todoData={todoData} setTodoData={setTodoData} />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+      </div>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import Form from "./components/Form";
+
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      conpleted: false,
+    };
+
+    //원래 있던 할 일에 새로운 할 일을 더해주기
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue(""); //입력란 텍스트 삭제
+  };
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+        </div>
+        <List todoData={todoData} setTodoData={setTodoData} />
+
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+      </div>
+    </div>
+  );
+}
